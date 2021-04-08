7a2a4c0e1927d340368abd626b3d6e962281d7ed
Rodrigo Vogt <rodrigo.vgt@gmail.com>
2021-04-08 15:16:08 -0300

    criação dos arquivos para o trab 02
    Arquivos do trab 02

--- JS/exemplo.js
+++ JS/exemplo.js
@@ -1,22 +1,17 @@
 var scene; //mundo virtual
-var camera; //area de visualizaÃ§Ã£o
+var camera; //area de visualização
 var renderer; //responsavel por renderizar tudo
 
 var elementos = [];
 
 var velocidade = 0.07;
 
-var criaCubo = function(){
-	let geometria = new THREE.BoxGeometry(5, 5, 5);
+var criaSisSolar = function(){
 
 	let red = 	new THREE.Color(1,0,0);
 	let green = new THREE.Color(0,1,0);
 	let blue = 	new THREE.Color(0,0,1);
 	let cores = [red, green, blue];
-
-
-
-
 
 	let materials = [
 		new THREE.MeshBasicMaterial({color: red}),
@@ -27,53 +22,107 @@ var criaCubo = function(){
 		new THREE.MeshBasicMaterial({color: blue})
 	];
 	
-	let cubo = new THREE.Mesh(geometria, materials);
-	cubo.position.x = -3;
-	elementos["cubo1"] = cubo;
+	let sol = new THREE.Mesh(new THREE.SphereGeometry(8, 32, 32), new THREE.MeshBasicMaterial({color: 0xffff00}));
+	sol.position.x = -10;
+	elementos["sol"] = sol;
 
-	let cubo2 = new THREE.Mesh(new THREE.BoxGeometry(5, 5, 5), materials);
-	cubo2.position.x= 8;
-	elementos["cubo2"] = cubo2;	
+	let pivot = new THREE.Group();
+	sol.add(pivot);
+	pivot.position.x = sol.position.x+8; //posicao do sol
+
+	elementos["pivot"] = pivot;
+
+	let terra = new THREE.Mesh(new THREE.SphereGeometry(1.5, 6, 6), new THREE.MeshBasicMaterial({color: blue}));
+	pivot.add(terra);
+	terra.position.x += pivot.position.x+26;
+	elementos["terra"] = terra;
+
+	let marte = new THREE.Mesh(new THREE.SphereGeometry(2, 6, 6), new THREE.MeshBasicMaterial({color: red}));
+	pivot.add(marte);
+	marte.position.x += pivot.position.x+20;
+
+
+	// let cubo2 = new THREE.Mesh(new THREE.BoxGeometry(10, 3, 3), materials);
+	// cubo2.position.x= 7;
+	// elementos["cubo2"] = cubo2;	
 	
 	
-	scene.add(cubo2);
-	scene.add(cubo);
+	scene.add(sol);
+	//scene.add(pivot);
+
+};
+
+var criaMonstro = function (){
 
 };
 
 
+
 var init = function (){
 	scene = new THREE.Scene();
-	camera = new THREE.PerspectiveCamera(40, window.innerWidth/window.innerHeight, 1, 100);
+	camera = new THREE.PerspectiveCamera(40, window.innerWidth/window.innerHeight, 1, 150);
 	
 	renderer = new THREE.WebGLRenderer();
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	document.body.appendChild(renderer.domElement);
 	
-	camera.position.z =50;
+	camera.position.z =80;
 	camera.position.x = 0;
 	camera.position.y = 2;
 	
-	criaCubo();
+	criaSisSolar();
 	
+
 	animation();
+
 
 	document.addEventListener('keydown', apertouButao);
 	document.addEventListener('keyup', soltouBotao);
+
+	//metodos do mouser
+	document.addEventListener('mousewheel', onMouseWheel);
+
 	
 };
 
-var r = false;
-var space = false;
+
+var onMouseWheel = function (e){
+
+	//for (let el in elementos){
+		elementos["sol"].scale.x+= (e.deltaY > 0)?-0.1:0.1;
+		elementos["sol"].scale.y+= (e.deltaY > 0)?-0.1:0.1;
+		elementos["sol"].scale.z+= (e.deltaY > 0)?-0.1:0.1;
+	//}
+	//Mesma coisa que o anterior 
+	//if (e.deltaY > 0){
+		
+	// 	elementos["cubo2"].scale.y-=0.1;
+	// 	elementos["cubo2"].scale.z-=0.1;
+	// } else {
+	// 	elementos["cubo2"].scale.x+=0.1;
+	// 	elementos["cubo2"].scale.y+=0.1;
+	// 	elementos["cubo2"].scale.z+=0.1;
+	// }
+
+}
+
+
+
+var key_r = true;
+var key_space = true;
+var key_q = true;
 
 var soltouBotao = function(e){
 	console.log('soltou '+ e.keyCode);
 
-	if (e.keyCode == 82){
-		r = false;
+	if (e.keyCode == 82){ //r
+		key_r = false;
 	}
-	if (e.keyCode == 32){
-		space = false;
+	if (e.keyCode == 32){ //espaço
+		key_space = false;
+	}
+	if (e.keyCode == 81){ //espaço
+		key_q = false;
 	}
 }
 
@@ -81,25 +130,42 @@ var soltouBotao = function(e){
 var apertouButao =  function(e){
 	console.log(e.keyCode);
 
-	if (e.keyCode == 82){
-		r = true;
+	if (e.keyCode == 82){ //r
+		key_r = true;
 	}
-	if (e.keyCode == 32){
-		space = true;
+	if (e.keyCode == 32){ // space
+		key_space = true;
+	}
+
+	if (e.keyCode == 81){ // q
+		key_q = true;		
+	}
+
+	if (e.keyCode == 189){ //-
+		elementos["terra"].scale.x-=0.1;
+		elementos["cubo2"].scale.y-=0.1;
+		elementos["cubo2"].scale.z-=0.1;
+	}
+	if (e.keyCode == 187){ // +
+		elementos["terra"].scale.x+=0.1;
+		elementos["terra"].scale.y+=0.1;
+		elementos["terra"].scale.z+=0.1;
 	}
 }
 
 var count =0; 
 var animation = function (){
-	requestAnimationFrame(animation); //adiciona o mÃ©todo na fila de renderizaÃ§Ã£o
+	requestAnimationFrame(animation); //adiciona o método na fila de renderização
 
-	if (space){
-		elementos["cubo2"].position.x += 0.1;
+	if (key_space){
+		elementos["sol"].rotation.y += 0.01;
 	}
-	if (r){
-		elementos["cubo2"].rotation.x += 0.01;
-		elementos["cubo2"].rotation.z += 0.01;
-		
+	if (key_r){
+		elementos["terra"].rotation.x += 0.01; //angulos de Euler
+		elementos["terra"].rotation.z += 0.01;
+	}
+	if (key_q){
+		elementos["pivot"].rotation.z +=0.01;
 	}
 	
 	//if (++count >= 30){
@@ -109,8 +175,11 @@ var animation = function (){
 	// 	elementos["cubo2"].rotation.x += 0.01;
 	// 	elementos["cubo2"].rotation.z += 0.01;
 	// //	count = 0;
-	//}
+	
+
+
 	renderer.render(scene, camera); //tira uma foto do estado e mostra na tela
 }
+
 
 window.onload = this.init
